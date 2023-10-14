8040a60114309a80bb4ff22b1adc2a99096c1433
song-ingyu <songik@kakao.com>
2023-10-14 23:10:54 +0900

    1차 과제 모음(2차 수정) -No1 최적화

--- Week5/functionEx1.py
+++ Week5/functionEx1.py
@@ -1,3 +1,15 @@
+def get_dest(location):
+    return dest_dict.get(location)
+
+
+def get_class(class_type):
+    return class_dict.get(class_type)
+
+
+def get_seat(seat_type):
+    return seat_dict.get(seat_type)
+
+
 dest_dict = {
     "춘천": 5000,
     "부산": 30000,
@@ -5,28 +17,18 @@ dest_dict = {
     "수원": 10000
 }
 
-def get_dest(location):
-    return dest_dict.get(location)
+class_dict = {
+    "KTX": 10000,
+    "새마을호": 5000,
+    "무궁화호": 3000,
+}
 
-def classType(classType):
-    if classType == "KTX":
-        won = 10000
-    elif classType == "새마을호":
-        won = 5000
-    elif classType == "무궁화호":
-        won = 3000
-    return won
-
-
-def seatType(seatType):
-    if seatType == "입석":
-        won = -2000
-    else:
-        won = 0
-    return won
-
+seat_dict = {
+    "입석": -2000,
+    "좌석": 0
+}
 
 money = get_dest(input("춘천(운임 : 5000원) 부산(운임 :30000원) 대구(운임 20000원) 수원(운임 10000) 중 목적지를 한곳 입력하세요 : "))
-money += classType(input("KTX(10000원 추가) / 새마을호(5000원 추가) / 무궁화호(3000원 추가) 중 하나를 입력하세요 : "))
-money += seatType(input("좌석 / 입석(2000원 할인)중 하나를 입력하세요 : "))
+money += get_class(input("KTX(10000원 추가) / 새마을호(5000원 추가) / 무궁화호(3000원 추가) 중 하나를 입력하세요 : "))
+money += get_seat(input("좌석 / 입석(2000원 할인)중 하나를 입력하세요 : "))
 print("지불하실 운임은 " + str(money) + "원 입니다.")
