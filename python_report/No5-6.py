3354dce4981c3adf2d5fa12d90fd6fe6468d8ca9
song-ingyu <songik@kakao.com>
2023-10-15 15:55:02 +0900

    최적화

--- python_report/No5-6.py
+++ python_report/No5-6.py
@@ -10,6 +10,10 @@ for id, mile in dic.items():
     print(f"아이템: {id}, 마일리지: {mile}")
 
 print() # No6
-key_max = max(dic.keys(), key=(lambda k: dic[k]))
-value_max = dic[key_max]
-print(f"{key_max} 님이 {value_max} 으로 가장 높은 점수입니다.")
+
+max_id = max(dic, key=dic.get)
+print(f"{max_id} 님이 {dic[max_id]} 으로 가장 높은 점수입니다.")
+
+# key_max = max(dic.keys(), key=(lambda k: dic[k]))
+# value_max = dic[key_max]
+# print(f"{key_max} 님이 {value_max} 으로 가장 높은 점수입니다.")
