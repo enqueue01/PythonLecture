ec90b367c28470dcf6a00e014a88323b0f957568
song-ingyu <songik@kakao.com>
2023-10-20 21:23:59 +0900

    파이썬 과제

--- python_report_2/No4.py
+++ python_report_2/No4.py
@@ -1,7 +1,9 @@
 def fact(num=5):
     result = 1
-    if __name__ == '__main__':
-        num = 5
     for i in range(1, num + 1):
         result *= i
     return result
+
+
+if __name__ == '__main__':
+    print(fact())
